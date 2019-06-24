c25342fb94203c99a67eeb81603a15a2992a70dc
d_yama <7d.yama@gmail.com>
2019-06-24 17:40:00 +0900

    [add]CPU RAMの書き込みのテストメソッドを追加した

--- SharpNES.standard.test/CpuRam.test.cs
+++ SharpNES.standard.test/CpuRam.test.cs
@@ -16,7 +16,7 @@ namespace SharpNES.standard.test
         public void 初期化時メモリは0で埋められる(Address address)
         {
             var data = sut.Read(address);
-            data.Is((byte)0x0000);
+            data.Is((byte) 0x0000);
         }
 
         [Theory]
@@ -24,10 +24,26 @@ namespace SharpNES.standard.test
         [InlineData(0x0801)]
         public void 範囲外のアドレスを指定したときは例外が発生する(Address address)
         {
-            Assert.Throws<ArgumentException>(() =>
-            {
-                sut.Read(address);
-            });
+            Assert.Throws<ArgumentException>(() => { sut.Read(address); });
+        }
+
+        [Theory]
+        [InlineData(0x0000, 0x10)]
+        [InlineData(0x0400, 0x00)]
+        [InlineData(0x07FF, 0xFF)]
+        public void アドレスを指定してメモリにデータを書き込むことができる(Address address, byte data)
+        {
+            sut.Write(address, data);
+            var readData = sut.Read(address);
+            readData.Is(data);
+        }
+
+        [Theory]
+        [InlineData(0x0800)]
+        [InlineData(0x0801)]
+        public void 範囲外のアドレスを指定して書き込もうとすると例外が発生する(Address address)
+        {
+            Assert.Throws<ArgumentException>(() => sut.Write(address, 0x00));
         }
     }
 }
