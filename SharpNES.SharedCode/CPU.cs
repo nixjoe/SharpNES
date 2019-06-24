680b14f14ac5df474ee2bcb651d67e5d357deab5
d_yama <7d.yama@gmail.com>
2019-06-24 11:38:39 +0900

    [fix]共通コードを配置するプロジェクトの名前を修正した

---
+++ SharpNES.SharedCode/CPU.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace SharpNES.SharedCode
+{
+	class CPU
+	{
+		private class Registers
+		{
+			public byte A { get; set; }
+			public byte X { get; set; }
+			public byte Y { get; set; }
+			public byte S { get; set; }
+			public ushort PC { get; set; }
+		}
+
+		private Registers registers;
+
+		public CPU()
+		{
+			registers = new Registers();
+		}
+
+		public void Reset()
+		{
+			// TODO プログラムカウンタの初期値は0xFFFCから読んでくる
+			registers.S = 0xFD;
+			registers.A = 0;
+			registers.X = 0;
+			registers.Y = 0;
+		}
+	}
+}
