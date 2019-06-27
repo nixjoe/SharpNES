d1103d801df6540d928aefe2b9b5cc8a6e48ec06
dykarohora <7d.yama@gmail.com>
2019-06-28 07:54:52 +0900

    [add]CPUの命令実行部を実装した

--- SharpNES.SharedCode/CPU.cs
+++ SharpNES.SharedCode/CPU.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
 using Address = System.UInt16;
 
 namespace SharpNES.SharedCode
@@ -49,6 +50,22 @@ namespace SharpNES.SharedCode
             public bool Break { get; set; }
             public bool Overflow { get; set; }
             public bool Negative { get; set; }
+
+            public void SetStatusFlags(byte flags)
+            {
+                Carry = BitSet(flags, 0);
+                Zero = BitSet(flags, 1);
+                Interrupt = BitSet(flags, 2);
+                Decimal = BitSet(flags, 3);
+                Break = BitSet(flags, 4);
+                Overflow = BitSet(flags, 5);
+                Negative = BitSet(flags, 6);
+            }
+
+            private bool BitSet(byte value, int index)
+            {
+                return (value & (1 << index)) == 1;
+            }
         }
 
         private ICpuBus bus;
@@ -73,16 +90,79 @@ namespace SharpNES.SharedCode
             registers.A = 0;
             registers.X = 0;
             registers.Y = 0;
+            statusFlags.SetStatusFlags((byte)0x24);
         }
 
-        public void Run()
+        public int ExecuteInstruction()
         {
             // TODO 割り込み処理
-            // TODO オペコードの取得
-            // TODO オペコードから命令種別とアドレッシングモードを解析する
-            // TODO アドレッシングモードから処理対象のアドレスを解析
-            // TODO 命令の実行
+
+            var opcode = fetch(registers.PC);
+            var instruction = instructionSet[opcode];
+            var address = getAddress(instruction.AddressingMode);
+            instruction.InstructionFunc(instruction.AddressingMode, address);
+            // TODO サイクルの調整
+            return instruction.Cycle;
         }
 
+        private byte fetch(Address address)
+        {
+            registers.PC++;
+            return bus.Read(address);
+        }
+
+        private Address fetchWord(Address address)
+        {
+            var lower = fetch(address);
+            var upper = fetch((Address)(address + 1));
+            return (Address)(upper << 8 | lower);
+        }
+
+        private Address getAddress(AddressingMode mode)
+        {
+            switch (mode)
+            {
+                case AddressingMode.Absolute:
+                    return fetchWord(registers.PC);
+                case AddressingMode.AbsoluteX:
+                    {
+                        var address = fetchWord(registers.PC);
+                        return (Address)(address + registers.X);
+                        // TODO サイクル調整
+                    }
+                case AddressingMode.AbsoluteY:
+                    {
+                        var address = fetchWord(registers.PC);
+                        return (Address)(address + registers.Y);
+                        // TODO サイクル調整
+                    }
+                case AddressingMode.Accumulator:
+                    return 0x0000;
+                case AddressingMode.Immediate:
+                    return fetch(registers.PC);
+                case AddressingMode.Implied:
+                    return 0x0000;  // Impliedはアドレスを使用しないのでダミーを返す
+                case AddressingMode.IndexedIndirect:
+                    break;
+                case AddressingMode.Indirect:
+                    break;
+                case AddressingMode.IndirectIndexed:
+                    break;
+                case AddressingMode.Relative:
+                    {
+                        var offset = fetch(registers.PC);
+                        return (Address)(registers.PC + offset);
+                    }
+                case AddressingMode.ZeroPage:
+                    break;
+                case AddressingMode.ZeroPageX:
+                    break;
+                case AddressingMode.ZeroPageY:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(mode), mode, null);
+            }
+            throw new NotImplementedException();
+        }
     }
 }
