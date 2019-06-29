49e5e9f2c98f099cb43c1608390f4884c4b2a4a9
dykarohora <7d.yama@gmail.com>
2019-06-29 18:58:10 +0900

    [add]VRAMへの書き込み処理を実装した

--- SharpNES.SharedCode/PPU.cs
+++ SharpNES.SharedCode/PPU.cs
@@ -1,15 +1,44 @@
-﻿using System.Xml;
+﻿using System;
+using System.Xml;
 using Address = System.UInt16;
 
 namespace SharpNES.SharedCode
 {
     public class PPU
     {
+        internal class ControlRegister
+        {
+            private byte register;
+
+            public void SetFrags(byte data)
+            {
+                    register = data;
+            }
+
+            /// <summary>
+            /// PPUDATA($2007)に書き込んだ時のアドレスインクリメント値
+            /// </summary>
+            public Address AddressIncrement => ((register >> 2) & 0x01) == 1 ? (ushort) 0x20 : (ushort) 0x01;
+        }
+
+        internal class MaskRegister
+        {
+            private byte register;
+
+            public void SetFlags(byte data)
+            {
+                register = data;
+            }
+        }
+
         private int cycle;
 
 
         private RAM vram;
         private RAM spriteRam;
+
+        private ControlRegister controlRegister = new ControlRegister();
+        private MaskRegister maskRegister = new MaskRegister();
 
         // VRAMアドレス
         private bool isAlreadySetUpperVramAddress = false;
@@ -24,8 +53,15 @@ namespace SharpNES.SharedCode
 
         public void Write(Address address, byte data)
         {
-            if (address == 0x2000 || address == 0x2001)
+            if (address == 0x2000)
             {
+                controlRegister.SetFrags(data);
+                return;
+            }
+
+            if (address == 0x2001)
+            {
+                maskRegister.SetFlags(data);
                 return;
             }
 
@@ -52,6 +88,7 @@ namespace SharpNES.SharedCode
 
             if (address == 0x2007)
             {
+                WriteVramData(data);
                 return;
             }
         }
@@ -75,6 +112,36 @@ namespace SharpNES.SharedCode
             currentVramAddress = (Address)(data << 8);
             isAlreadySetUpperVramAddress = true;
             isCompleteVramAddress = false;
+        }
+
+        /// <summary>
+        /// PPUが持つVRAMへデータを書き込む。
+        /// </summary>
+        /// <param name="data"></param>
+        private void WriteVramData(byte data)
+        {
+            if (currentVramAddress >= 0x2000)
+            {
+                if (0x3f00 <= currentVramAddress && currentVramAddress < 0x4000)
+                {
+                    // パレットテーブルへの書き込み
+                }
+                else
+                {
+                    // ネームテーブル、属性テーブルへの書き込み
+                    // 0x3000 - 0x3EFF間のミラーリングを考慮
+                     var address = 0x3000 <= currentVramAddress && currentVramAddress < 0x3F00
+                        ? (Address)(currentVramAddress - 0x3000)
+                        : (Address)(currentVramAddress - 0x2000);
+                    vram.Write(address, data);
+                }
+            }
+            else
+            {
+                // TODO キャラクタRAMへの書き込み
+            }
+
+            currentVramAddress += controlRegister.AddressIncrement;
         }
     }
 }
